3062ca2d0fecaab828173693373d1b6610e339ae
Rohan Mishra <rohan@crazydeveloper.fail>
2020-10-11 13:18:56 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,16 @@
 set -e
 
 rm -rf /var/run/dbus.pid
+rm -ff /run/dbus/pid
+
 #mkdir -p /var/run/dbus
 
 dbus-uuidgen --ensure
 dbus-daemon --system
 
-avahi-daemon --daemonize --no-chroot
+pulseaudio --log-level=1 &
+/usr/lib/bluetooth/bluetoothd --plugin=a2dp -n &
 
+avahi-daemon --daemonize --no-chroot
+./bin/simple-bluetooth-agent.sh &
 su-exec shairport-sync shairport-sync $@
